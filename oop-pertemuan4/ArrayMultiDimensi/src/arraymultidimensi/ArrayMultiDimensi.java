ae9d19bcc52e2897c73a385a0e80c894034664fe
lendriana30 <yulitadalia@gmail.com>
2025-04-12 14:50:27 +0800

    Add files via upload

---
+++ oop-pertemuan4/ArrayMultiDimensi/src/arraymultidimensi/ArrayMultiDimensi.java
@@ -0,0 +1,26 @@
+package arraymultidimensi;
+
+/**
+ *
+ * @author Lendriana
+ * TGL 12 April 2025
+ */
+public class ArrayMultiDimensi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String[][] meja = new String [3][2];
+       
+       meja[0][0] = "Meja A";
+       meja[0][1] = "Meja B";
+       
+       meja[1][0] = "Meja C";
+       meja[1][1] = "Meja D";
+       
+       meja[2][0] = "Meja E";
+       meja[2][1] = "Meja F";
+    }
+    
+}
